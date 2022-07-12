eb13733a5b29db9ddd9244ac7d85c6fb37d2383a
evidencefrank <evidencefrank@outlook.com>
2022-07-12 18:46:36 +0200

    added vue3 and tailwind, configured to use vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+//Vue Init
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import EmployeesComponent from "./components/employees/EmployeesComponent.vue";
+
+createApp({
+    components: {
+        EmployeesComponent,
+    }
+}).mount('#app');
