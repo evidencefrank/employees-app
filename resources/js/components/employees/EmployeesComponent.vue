60be97b0d0deaa82631d3beb6a6a008d902ba650
evidencefrank <evidencefrank@outlook.com>
2022-07-14 02:20:19 +0200

    minor fix

--- resources/js/components/employees/EmployeesComponent.vue
+++ resources/js/components/employees/EmployeesComponent.vue
@@ -7,9 +7,6 @@
         <div id="slide-over-container" class="fixed inset-0 w-full h-full" :class="[isSlideOverOpen ? '' : 'invisible']">
             <div id="slide-over-bg" class="absolute duration-500 ease-out transition-all inset-0 w-full h-full bg-gray-900" :class="[isSlideOverOpen ? 'opacity-50' : 'opacity-0']"></div>
             <div id="slide-over" class="absolute w-1/3 rounded-[60px] duration-500 ease-out h-full bg-gray-700 left-2 top-0" :class="[isSlideOverOpen ? '' : '-translate-x-full']">
-<!--                <div @click="isSlideOverOpen = !isSlideOverOpen" class="w-10 h-10 text-gray-600 flex items-center justify-center absolute top-0 right-0 mt-2 mr-12">-->
-<!--                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>-->
-<!--                </div>-->
 
                 <employee-create v-if="isCreate" @closeForm="closeForm"></employee-create>
                 <employee-edit v-else :id="employeeId" @closeForm="closeForm"></employee-edit>
